6a103952f98087191ca24d208fbfbf3bcd81c1ad
maany <imptodefeat@gmail.com>
2016-11-19 13:50:10 +0530

    add date comparisions to obtain monthly PR's

--- formatter.py
+++ formatter.py
@@ -1,8 +1,22 @@
 import argparse
-import datetime as dt;
+import datetime as dt
+from dateutil import relativedelta as rdelta
+
 parser = argparse.ArgumentParser(description='This module will parse pull request and commit history for openmrs repositories and return monthly statistics')
 parser.add_argument('-p','--pulls', help="pull request dates as json", required=True)
 args = parser.parse_args()
 dates = args.pulls.replace("\"","").split()
 dates_list = [dt.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ').date() for date in dates]
-print(dates_list)
+# print(dates_list)
+reference = dt.date(2015,11,1)
+
+pr_list = [0] * 12
+pr_list = [0 for i in xrange(12)]
+
+
+for date in dates_list:
+    rd = rdelta.relativedelta(date,reference)
+    if rd.years ==0 and rd.months>=0:
+        pr_list[rd.months] += 1
+    #print "{0.years} years and {0.months} months".format(rd)
+print(pr_list)
