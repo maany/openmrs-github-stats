7712241f80f1f616ada12ac8ba3ffed72c3c11be
maany <imptodefeat@gmail.com>
2016-11-19 08:50:00 +0530

    parse repositories

--- openmrs-repo-metadata.sh
+++ openmrs-repo-metadata.sh
@@ -5,5 +5,12 @@ echo -n "Hey $user! Please enter your GitHub account password : "
 read -s password #silent read
 page=1
 result=$(curl --user "$user":"$password" -X GET "https://api.github.com/orgs/openmrs/repos?type=all&page=$page&per_page=100&sort=full_name" | jq .[].name)
-number_of_results=${#result[*]}
-echo $result
+arr=($result)
+number_of_results=${#arr[*]}
+echo ${arr}
+echo "$number_of_results"
+for (( i=0; i<${number_of_results}; i++ ));
+do
+    echo ${arr[$i]}
+    # obtain contribution details
+done
