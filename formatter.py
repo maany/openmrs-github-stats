eff7aa74282714ef424a459bf654acae8b327da7
maany <imptodefeat@gmail.com>
2016-11-19 12:50:04 +0530

    parse PR creation dates into list

--- formatter.py
+++ formatter.py
@@ -1,3 +1,8 @@
 import argparse
+import datetime as dt;
 parser = argparse.ArgumentParser(description='This module will parse pull request and commit history for openmrs repositories and return monthly statistics')
 parser.add_argument('-p','--pulls', help="pull request dates as json", required=True)
+args = parser.parse_args()
+dates = args.pulls.replace("\"","").split()
+dates_list = [dt.datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ').date() for date in dates]
+print(dates_list)
